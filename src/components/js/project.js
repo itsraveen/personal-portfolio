3673cb805bf2c927ce1661b9afbf2ec889654eb2
Raveen Prabhu <raveenprabhu99@gmail.com>
2024-05-03 21:37:13 +0800

    Style tabs in project page

--- src/components/js/project.js
+++ src/components/js/project.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import ProjectItem from './projectitem'; // Ensure the import matches the file name
-import projectData from '../../data/project-data'; // The path here must be correct
-import * as styles from '../css/projects.module.css'; // CSS import
+import ProjectItem from './projectitem';
+import projectData from '../../data/project-data';
+import * as styles from '../css/projects.module.css';
 
 const Projects = () => {
   const [filter, setFilter] = useState('all');
   
-  // The filter function should work correctly as long as projectData is an array of objects as shown.
   const filteredProjects = filter === 'all' 
     ? projectData 
     : projectData.filter(project => project.category === filter);
@@ -18,17 +17,15 @@ const Projects = () => {
           <h2>Projects</h2>
           <p>These are some of the projects that I created during my modules, hackathons and my own free time.</p>
         </div>
-        <div data-aos="fade-up">
-          <ul id="portfolio-flters" className={styles.portfolioFilters}>
-            <li onClick={() => setFilter('all')} className={filter === 'all' ? styles.filterActive : ''}>All</li>
-            <li onClick={() => setFilter('app')} className={filter === 'app' ? styles.filterActive : ''}>App</li>
-            <li onClick={() => setFilter('uiux')} className={filter === 'uiux' ? styles.filterActive : ''}>UI/UX</li>
-            <li onClick={() => setFilter('others')} className={filter === 'others' ? styles.filterActive : ''}>Others</li>
-          </ul>
+        <div className={styles.filterContainer} data-aos="fade-up">
+          <button onClick={() => setFilter('all')} className={filter === 'all' ? styles.filterActive : styles.filterButton}>All</button>
+          <button onClick={() => setFilter('app')} className={filter === 'app' ? styles.filterActive : styles.filterButton}>App</button>
+          <button onClick={() => setFilter('uiux')} className={filter === 'uiux' ? styles.filterActive : styles.filterButton}>UI/UX</button>
+          <button onClick={() => setFilter('others')} className={filter === 'others' ? styles.filterActive : styles.filterButton}>Others</button>
         </div>
         <div className={styles.portfolioContainer} data-aos="fade-up" data-aos-delay="100">
           {filteredProjects.map(project => (
-            <ProjectItem key={project.id} project={project} /> // Pass each project as a prop
+            <ProjectItem key={project.id} project={project} />
           ))}
         </div>
       </div>
