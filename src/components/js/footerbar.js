4eb429d136fa814c488b757b2b1229796a236c68
Raveen Prabhu <raveenprabhu99@gmail.com>
2024-09-01 13:43:14 +0800

    Fix footerbar

--- src/components/js/footerbar.js
+++ src/components/js/footerbar.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
-import * as styles from '../css/footerbar.module.css'; // Import CSS module
+import * as styles from '../css/footerbar.module.css';
 
 const FooterBar = () => {
   return (
-    <div className={styles.footerBar}> 
-      <div className={styles.content}> 
+    <div className={styles.footerBar}>
+      <div className={styles.content}>
         <h1>Raveen Prabhu</h1>
-        <p>Full-Stack Developer</p>
-        <div className={styles.icons}> 
-          <a href="https://www.linkedin.com/in/raveenprabhu/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-          <a href="https://github.com/itsraveen" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-          <a href="mailto:raveenprabhu99@gmail.com"><FaEnvelope /></a>
+        <h2>Full-Stack Developer</h2>
+        <div className={styles.icons}>
+          <a href="https://www.linkedin.com/in/raveenprabhu/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/itsraveen" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+          <a href="mailto:raveenprabhu99@gmail.com">
+            <FaEnvelope />
+          </a>
         </div>
         <p className={styles.builtby}>
           Built with React & Gatsby for Desktop (Mobile ver coming soon!).
-          Icons by <a href="https://icons8.com/" target="_blank" rel="noopener noreferrer"> Icons8</a>
+          Icons by <a href="https://icons8.com/" target="_blank" rel="noopener noreferrer">Icons8</a>
         </p>
       </div>
     </div>
