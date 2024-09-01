fb71d7bd3503cfca43c85febe4a107793a5728c9
Raveen Prabhu <raveenprabhu99@gmail.com>
2024-09-01 17:55:20 +0800

    Configure hero screen for mobile

--- src/components/js/navbar.js
+++ src/components/js/navbar.js
@@ -12,8 +12,8 @@ const Navbar = () => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
     };
-
-    handleResize();
+  
+    handleResize(); // Run the function on mount
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
@@ -23,18 +23,43 @@ const Navbar = () => {
   };
 
   return (
-    <nav id="navbar" className={styles.navbar}>
+    <nav className={styles.navbar}>
       <div className={styles.navbarLeft}>
         <Link to="/" className={styles.navbarLink}>
           <span className={styles.navbarBrand}>Raveen Prabhu</span>
         </Link>
       </div>
       {isMobile ? (
-        <div className={styles.navbarRight}>
-          <button className={styles.menuButton} onClick={toggleMenu}>
-            <Menu size={24} />
-          </button>
-        </div>
+        <>
+          <div className={styles.navbarRight}>
+            {/* <button className={styles.menuButton} onClick={toggleMenu}>
+              <Menu size={24} />
+            </button> */}
+
+            <button className={styles.menuButton} onClick={toggleMenu}>
+              ☰ {/* Temporary replacement for testing */}
+            </button>
+
+          </div>
+          {isMenuOpen && (
+            <div className={styles.mobileMenu}>
+              <ul className={styles.mobileNavbarMenu}>
+                <li className={styles.mobileNavbarItem}>
+                  <Link to="/" className={styles.navbarLink}>Home</Link>
+                </li>
+                <li className={styles.mobileNavbarItem}>
+                  <Link to="/about" className={styles.navbarLink}>About</Link>
+                </li>
+                <li className={styles.mobileNavbarItem}>
+                  <Link to="/experience" className={styles.navbarLink}>Experiences</Link>
+                </li>
+                <li className={styles.mobileNavbarItem}>
+                  <Link to="/projects" className={styles.navbarLink}>Projects</Link>
+                </li>
+              </ul>
+            </div>
+          )}
+        </>
       ) : (
         <div className={styles.navbarRight}>
           <ul className={styles.navbarMenu}>
@@ -48,24 +73,6 @@ const Navbar = () => {
               <Link to="/experience" className={styles.navbarLink}>Experiences</Link>
             </li>
             <li className={styles.navbarItem}>
-              <Link to="/projects" className={styles.navbarLink}>Projects</Link>
-            </li>
-          </ul>
-        </div>
-      )}
-      {isMobile && isMenuOpen && (
-        <div className={styles.mobileMenu}>
-          <ul className={styles.mobileNavbarMenu}>
-            <li className={styles.mobileNavbarItem}>
-              <Link to="/" className={styles.navbarLink}>Home</Link>
-            </li>
-            <li className={styles.mobileNavbarItem}>
-              <Link to="/about" className={styles.navbarLink}>About</Link>
-            </li>
-            <li className={styles.mobileNavbarItem}>
-              <Link to="/experience" className={styles.navbarLink}>Experiences</Link>
-            </li>
-            <li className={styles.mobileNavbarItem}>
               <Link to="/projects" className={styles.navbarLink}>Projects</Link>
             </li>
           </ul>
