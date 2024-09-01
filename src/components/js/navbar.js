5b086031b29313230875b57dec613a1f14159c75
Raveen Prabhu <raveenprabhu99@gmail.com>
2024-09-01 13:25:27 +0800

    Fix navbar for mobile

--- src/components/js/navbar.js
+++ src/components/js/navbar.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby';
+import { Menu } from 'lucide-react';
 
 import * as styles from '../css/navbar.module.css';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav id="navbar" className={styles.navbar}>
       <div className={styles.navbarLeft}>
@@ -11,22 +29,48 @@ const Navbar = () => {
           <span className={styles.navbarBrand}>Raveen Prabhu</span>
         </Link>
       </div>
-      <div className={styles.navbarRight}>
-        <ul className={styles.navbarMenu}>
-          <li className={styles.navbarItem}>
-            <Link to="/" className={styles.navbarLink}>Home</Link>
-          </li>
-          <li className={styles.navbarItem}>
-            <Link to="/about" className={styles.navbarLink}>About</Link>
-          </li>
-          <li className={styles.navbarItem}>
-            <Link to="/experience" className={styles.navbarLink}>Experiences</Link>
-          </li>
-          <li className={styles.navbarItem}>
-            <Link to="/projects" className={styles.navbarLink}>Projects</Link>
-          </li>
-        </ul>
-      </div>
+      {isMobile ? (
+        <div className={styles.navbarRight}>
+          <button className={styles.menuButton} onClick={toggleMenu}>
+            <Menu size={24} />
+          </button>
+        </div>
+      ) : (
+        <div className={styles.navbarRight}>
+          <ul className={styles.navbarMenu}>
+            <li className={styles.navbarItem}>
+              <Link to="/" className={styles.navbarLink}>Home</Link>
+            </li>
+            <li className={styles.navbarItem}>
+              <Link to="/about" className={styles.navbarLink}>About</Link>
+            </li>
+            <li className={styles.navbarItem}>
+              <Link to="/experience" className={styles.navbarLink}>Experiences</Link>
+            </li>
+            <li className={styles.navbarItem}>
+              <Link to="/projects" className={styles.navbarLink}>Projects</Link>
+            </li>
+          </ul>
+        </div>
+      )}
+      {isMobile && isMenuOpen && (
+        <div className={styles.mobileMenu}>
+          <ul className={styles.mobileNavbarMenu}>
+            <li className={styles.mobileNavbarItem}>
+              <Link to="/" className={styles.navbarLink}>Home</Link>
+            </li>
+            <li className={styles.mobileNavbarItem}>
+              <Link to="/about" className={styles.navbarLink}>About</Link>
+            </li>
+            <li className={styles.mobileNavbarItem}>
+              <Link to="/experience" className={styles.navbarLink}>Experiences</Link>
+            </li>
+            <li className={styles.mobileNavbarItem}>
+              <Link to="/projects" className={styles.navbarLink}>Projects</Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
